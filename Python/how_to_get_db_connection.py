b0ad8bdcce9f661148888402a8f861f199484c82
TazTornadoo <71925487+TazTornadoo@users.noreply.github.com>
2021-12-30 19:50:29 +0100

    added template to access database with Python

---
+++ Python/how_to_get_db_connection.py
@@ -0,0 +1,6 @@
+from data_storage import connection
+import pandas as pd
+
+#Down below you find a template to retrieve data in a dataframe and load it into a dataframe
+
+#df = pd.read_sql_query('''Write here your sql to access the data.''', connection)
